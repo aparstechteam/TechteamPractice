eed4370d163d1f15e721b4d76250012c4baee8f8
nasimulhasan <nasimulhasan513@gmail.com>
2021-03-17 23:01:30 +0600

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,164 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <div class="headline">
+      <h2>Budget Calculator</h2>
     </div>
-    <router-view/>
+    <div class="budget_container">
+      <h1 class="budget">0</h1>
+    </div>
+    <div>
+      <form class="input_boxes">
+        <input type="number" placeholder="Cost" />
+        <input type="text" placeholder="Description" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+    <div class="budget_calculations">
+      <div class="income">
+        <h2>Income</h2>
+        <ul class="income_list">
+          <li>
+            Buying
+
+            <span><ion-icon name="close-outline"></ion-icon></span>
+          </li>
+        </ul>
+      </div>
+      <div class="expenses">
+        <h2>Expenses</h2>
+        <ul class="expenses_list">
+          <li>
+            Buying
+            <span><ion-icon name="close-outline"></ion-icon></span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" >
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+$green: rgb(88, 194, 106);
+$light: rgba(255, 255, 255, 0.932);
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 
-    &.router-link-exact-active {
-      color: #42b983;
+  width: 100%;
+
+  font-family: "Roboto", sans-serif;
+
+  .headline {
+    background-color: rgb(33, 30, 46);
+    color: $light;
+    padding: 10px 15px;
+    text-align: center;
+    width: 100%;
+  }
+
+  .input_boxes {
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+    input {
+      padding: 10px;
+      border-radius: 5px;
+      margin-right: 5px;
+      border: 2px solid rgba(221, 220, 220, 0.548);
+      box-shadow: 2px 2px 2px rgba(179, 177, 177, 0.685);
+    }
+    input[type="number"] {
+      width: 100px;
+    }
+    button {
+      border: none;
+      outline: none;
+      padding: 12px 15px;
+      background-color: $green;
+      color: $light;
+      border-radius: 5px;
+      &:hover {
+        transform: translateY(-2px);
+        transition: transform 0.2s ease-in;
+        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+      }
+    }
+  }
+
+  .budget_container {
+    background-color: $green;
+    width: 100%;
+    height: 200px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .budget {
+      font-size: 60px;
+      font-weight: 300;
+      color: $light;
+    }
+  }
+  .budget_calculations {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    .income {
+      width: 250px;
+    }
+    .expenses {
+      width: 250px;
+    }
+    ul {
+      list-style-type: none;
+      margin-top: 10px;
+      li {
+        position: relative;
+        padding: 10px;
+        box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+        cursor: pointer;
+
+        &:hover {
+          box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.3);
+          span {
+            opacity: 1;
+          }
+        }
+
+        span {
+          position: absolute;
+          width: 30px;
+          height: 30px;
+          top: -15px;
+          right: -15px;
+          background: rgba(22, 18, 27, 0.966);
+          color: rgb(233, 69, 69);
+          border-radius: 50%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          opacity: 0;
+          transition: opacity 0.3s ease-in;
+        }
+      }
     }
   }
 }
